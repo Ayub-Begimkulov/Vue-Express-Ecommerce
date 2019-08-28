3bfeddb3efd6aeea264820131f39684eb4a005a8
Ayub Begimkulov <begimkulov963@gmail.com>
2019-08-28 18:02:31 +0300

    Added cors middleware

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
