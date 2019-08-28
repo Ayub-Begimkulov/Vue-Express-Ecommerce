39e4473cb1c953d8d9238bb6f5a8e120a5b359fa
Ayub Begimkulov <begimkulov963@gmail.com>
2019-08-28 18:24:09 +0300

    Configured all routes

--- client/src/router.js
+++ client/src/router.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Cart from './views/Cart';
+import Product from './views/Product';
+import Login from './views/Login';
+import Signup from './views/Signup';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -13,13 +17,29 @@ export default new Router({
       name: 'home',
       component: Home
     },
+
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/cart',
+      name: 'cart',
+      component: Cart
+    },
+
+    {
+      path: '/product/:id',
+      name: 'product',
+      component: Product
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
     }
   ]
-})
+});
