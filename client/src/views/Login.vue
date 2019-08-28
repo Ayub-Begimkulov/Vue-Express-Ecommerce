a6554133cb6e22f075faa7212d982907f6d3ba5f
Ayub Begimkulov <begimkulov963@gmail.com>
2019-08-28 19:07:55 +0300

    Made static Login and Signup pages

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,2 +1,23 @@
 <template>
+  <form class="bg-white shadow max-w-lg mx-auto rounded">
+    <h3 class="text-xl text-gray-900 font-bold bg-gray-200 p-4">Login</h3>
+
+    <div class="p-5">
+      <label class="flex flex-col mb-4">
+        Email
+        <input class="border rounded px-3 py-1 mt-2" type="email" />
+      </label>
+
+      <label class="flex flex-col mb-4">
+        Password
+        <input class="border rounded px-3 py-1 mt-2" type="password" />
+      </label>
+
+      <button class="block w-full bg-indigo-700 text-white font-bold rounded px-4 py-2">Sign up</button>
+    </div>
+  </form>
 </template>
+
+<script>
+export default {};
+</script>
--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,2 +1,35 @@
 <template>
+  <form class="bg-white shadow max-w-lg mx-auto rounded">
+    <h3 class="text-xl text-gray-900 font-bold bg-gray-200 p-4">Sign up</h3>
+
+    <div class="p-5">
+      <div class="flex">
+        <label class="w-1/2 flex flex-col pr-1 mb-4">
+          First name
+          <input class="border rounded px-3 py-1 mt-2" type="text" />
+        </label>
+
+        <label class="w-1/2 flex flex-col pl-1 mb-4">
+          Last name
+          <input class="border rounded px-3 py-1 mt-2" type="text" />
+        </label>
+      </div>
+
+      <label class="flex flex-col mb-4">
+        Email
+        <input class="border rounded px-3 py-1 mt-2" type="email" />
+      </label>
+
+      <label class="flex flex-col mb-4">
+        Password
+        <input class="border rounded px-3 py-1 mt-2" type="password" />
+      </label>
+
+      <button class="block w-full bg-indigo-700 text-white font-bold rounded px-4 py-2">Sign up</button>
+    </div>
+  </form>
 </template>
+
+<script>
+export default {};
+</script>
