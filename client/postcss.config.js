7b8ce84c6d3fc8b2812758ce97195718c11bcaa5
Ayub Begimkulov <begimkulov963@gmail.com>
2019-08-28 18:18:23 +0300

    Added tailwindcss to the project

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,5 +1,6 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
-}
+  plugins: [tailwindcss('./tailwind.config.js'), autoprefixer]
+};
