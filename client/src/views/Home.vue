1d9070ca413c073f827b8bc4904e0c9ff3d0cf05
Ayub Begimkulov <begimkulov963@gmail.com>
2019-08-31 17:23:24 +0300

    Refactored code, chaged from fetch to axios and added product sending trough router-link

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,60 +1,70 @@
 <template>
   <div class="md:flex">
-    <div class="bg-white self-start rounded mx-2 mb-8 md:m-0 md:w-1/4 p-6">
-      <h3 class="uppercase mb-4">Categories</h3>
-      <label
-        v-for="(category, index) in categories"
-        :key="`category-${index}`"
-        class="flex items-center mb-2"
-      >
-        <input class="mr-2" type="checkbox" :value="category" v-model="filtersApplied.categories" />
-        {{category}}
-      </label>
+    <aside class="bg-white self-start rounded mx-2 mb-8 md:m-0 md:w-1/4 p-6">
+      <div class="flex md:block">
+        <div class="w-1/2 md:w-full">
+          <h3 class="text-lg mb-4">Categories</h3>
+          <label
+            v-for="(category, index) in categories"
+            :key="`category-${index}`"
+            class="flex items-center mb-2"
+          >
+            <input
+              class="mr-2"
+              type="checkbox"
+              :value="category"
+              v-model="filtersApplied.categories"
+            />
+            {{category}}
+          </label>
+        </div>
 
-      <h3 class="uppercase my-4">Brands</h3>
-      <label
-        v-for="(brand, index) in brands"
-        :key="`brand-${index}`"
-        class="flex items-center mb-2"
-      >
-        <input class="mr-2" type="checkbox" :value="brand" v-model="filtersApplied.brands" />
-        {{brand}}
-      </label>
+        <div class="w-1/2 md:w-full">
+          <h3 class="text-lg mb-4 md:my-4">Brands</h3>
+          <label
+            v-for="(brand, index) in brands"
+            :key="`brand-${index}`"
+            class="flex items-center mb-2"
+          >
+            <input class="mr-2" type="checkbox" :value="brand" v-model="filtersApplied.brands" />
+            {{brand}}
+          </label>
+        </div>
+      </div>
 
-      <h3 class="uppercase my-4">Price</h3>
+      <h3 class="text-lg my-4">Price</h3>
       <select class="focus:outline-none p-1" v-model="priceSorting">
         <option value>None</option>
         <option>Low to high</option>
         <option>High to low</option>
       </select>
-    </div>
+    </aside>
 
     <div class="md:w-3/4 flex flex-wrap">
-      <div v-for="product in filteredProducts" class="w-1/2 xl:w-1/3 px-2 md:pl-5 mb-5">
-        <div class="bg-white rounded hover:shadow-md p-6">
-          <router-link :to="`/product/${product._id}`">
-            <img
-              class="mb-4"
-              src="https://de9luwq5d40h2.cloudfront.net/catalog/product/large_image/43_41435100025.jpg"
-              alt="product"
-            />
-          </router-link>
+      <div
+        v-for="product in filteredProducts"
+        :key="product._id"
+        class="w-1/2 xl:w-1/3 px-2 md:pl-5 mb-5"
+      >
+        <router-link
+          :to="{ name: 'Product', params: {id: product._id, productData: product}}"
+          class="block bg-white rounded shadow hover:shadow-md-y"
+        >
+          <img :src="product.img" alt="product" />
 
-          <h3 class="font-bold hover:underline mb-3">
-            <router-link :to="`/product/${product._id}`">{{product.title}}</router-link>
-          </h3>
-
-          <div class="flex justify-between items-center">
-            <div class="font-bold">${{product.price}}</div>
-            <button class="bg-indigo-700 text-white font-bold rounded px-4 py-1">Add to cart</button>
+          <div class="p-4 sm:p-6">
+            <h3 class="font-bold hover:underline mb-3">{{product.title}}</h3>
+            <div class="font-bold mb-3 sm:mb-0">${{product.price}}</div>
           </div>
-        </div>
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Home',
   data() {
@@ -105,11 +115,9 @@ export default {
   },
 
   created() {
-    fetch('http://localhost:3000/api/products')
-      .then(res => res.json())
-      .then(data => {
-        this.products = data;
-      });
+    axios.get('http://localhost:3000/api/products').then(({ data }) => {
+      this.products = data;
+    });
   },
 
   methods: {
