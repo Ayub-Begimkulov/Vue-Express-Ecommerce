46a5c6998ddbcaeeb14c7733d7d21eac8b5891a0
Ayub Begimkulov <begimkulov963@gmail.com>
2019-08-31 11:01:49 +0300

    Added auth

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,23 +1,66 @@
 <template>
-  <form class="bg-white shadow max-w-lg mx-auto rounded">
+  <form @submit.prevent="login" class="bg-white shadow max-w-lg mx-auto rounded">
     <h3 class="text-xl text-gray-900 font-bold bg-gray-200 p-4">Login</h3>
 
     <div class="p-5">
       <label class="flex flex-col mb-4">
         Email
-        <input class="border rounded px-3 py-1 mt-2" type="email" />
+        <input class="border rounded px-3 py-1 mt-2" type="email" v-model="email" required />
       </label>
 
       <label class="flex flex-col mb-4">
         Password
-        <input class="border rounded px-3 py-1 mt-2" type="password" />
+        <input
+          class="border rounded px-3 py-1 mt-2"
+          type="password"
+          v-model="password"
+          required
+        />
       </label>
 
       <button class="block w-full bg-indigo-700 text-white font-bold rounded px-4 py-2">Sign up</button>
+
+      <div v-if="err" class="bg-red-600 text-white text-sm p-3 mt-4">{{err}}</div>
     </div>
   </form>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: '',
+      err: null
+    };
+  },
+
+  methods: {
+    login() {
+      axios
+        .post(
+          'http://localhost:3000/api/auth/login',
+          {
+            email: this.email,
+            password: this.password
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+        )
+        .then(({ data }) => {
+          localStorage.setItem('token', data.token);
+          this.$router.push('/');
+        })
+        .catch(err => {
+          this.err = err.response.data.message;
+        });
+    }
+  }
+};
 </script>
--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,35 +1,91 @@
 <template>
-  <form class="bg-white shadow max-w-lg mx-auto rounded">
+  <form @submit.prevent="signup" class="bg-white shadow max-w-lg mx-auto rounded">
     <h3 class="text-xl text-gray-900 font-bold bg-gray-200 p-4">Sign up</h3>
 
     <div class="p-5">
       <div class="flex">
         <label class="w-1/2 flex flex-col pr-1 mb-4">
           First name
-          <input class="border rounded px-3 py-1 mt-2" type="text" />
+          <input
+            class="border rounded px-3 py-1 mt-2"
+            type="text"
+            v-model="firstName"
+            required
+          />
         </label>
 
         <label class="w-1/2 flex flex-col pl-1 mb-4">
           Last name
-          <input class="border rounded px-3 py-1 mt-2" type="text" />
+          <input
+            class="border rounded px-3 py-1 mt-2"
+            type="text"
+            v-model="lastName"
+            required
+          />
         </label>
       </div>
 
       <label class="flex flex-col mb-4">
         Email
-        <input class="border rounded px-3 py-1 mt-2" type="email" />
+        <input class="border rounded px-3 py-1 mt-2" type="email" v-model="email" required />
       </label>
 
       <label class="flex flex-col mb-4">
         Password
-        <input class="border rounded px-3 py-1 mt-2" type="password" />
+        <input
+          class="border rounded px-3 py-1 mt-2"
+          type="password"
+          v-model="password"
+          required
+        />
       </label>
 
       <button class="block w-full bg-indigo-700 text-white font-bold rounded px-4 py-2">Sign up</button>
+
+      <div v-if="err" class="bg-red-600 text-white text-sm p-3 mt-4">{{err}}</div>
     </div>
   </form>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  name: 'Signup',
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      err: null
+    };
+  },
+
+  methods: {
+    signup() {
+      axios
+        .post(
+          'http://localhost:3000/api/auth/signup',
+          {
+            firstName: this.firstName,
+            lastName: this.lastName,
+            email: this.email,
+            password: this.password
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+        )
+        .then(({ data }) => {
+          localStorage.setItem('token', data.token);
+        })
+        .catch(err => {
+          this.err = err.response.data.message;
+        });
+    }
+  }
+};
 </script>
