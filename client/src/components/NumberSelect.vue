dec81ed4817e3e012a1afb977d297664e768da98
Ayub Begimkulov <begimkulov963@gmail.com>
2019-09-01 16:09:05 +0300

    Made a number select component

---
+++ client/src/components/NumberSelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <select
+    @change="$emit('change', value)"
+    class="w-12 focus:outline-none p-1"
+    v-model.number="value"
+  >
+    <option v-for="i in range" :key="`option-${i}`">{{i}}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'NumberSelect',
+  props: {
+    range: {
+      type: Number,
+      default: 10
+    },
+    defaultValue: {
+      type: Number,
+      default: 1
+    }
+  },
+
+  data() {
+    return {
+      value: this.defaultValue
+    };
+  }
+};
+</script>
