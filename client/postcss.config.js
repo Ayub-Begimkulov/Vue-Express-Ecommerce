e5440ec73e75905a06d240f96b78bf38b1a133f3
Ayub Begimkulov <begimkulov963@gmail.com>
2019-09-04 08:26:12 +0300

    Added purgecss

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,24 @@
 const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), autoprefixer]
+  plugins: [
+    tailwindcss('./tailwind.config.js'),
+    purgecss({
+      content: ['./public/index.html', './src/**/*.vue'],
+      extractors: [
+        {
+          extractor: class TailwindExtractor {
+            static extract(content) {
+              return content.match(/[A-z0-9-:\/]+/g) || [];
+            }
+          },
+
+          extensions: ['html', 'vue']
+        }
+      ]
+    }),
+    autoprefixer
+  ]
 };
