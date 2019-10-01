7bc0c273004555a9ca875ebc50721906ecedfdf0
Ayub Begimkulov <begimkulov963@gmail.com>
2019-10-01 21:26:13 +0300

    made FormInput component

--- client/src/components/AuthForm.vue
+++ client/src/components/AuthForm.vue
@@ -7,41 +7,26 @@
 
     <div class="p-5">
       <div v-if="isSignup" class="flex">
-        <label class="w-1/2 flex flex-col pr-1 mb-4">
-          First name
-          <input
-            class="border rounded px-3 py-1 mt-2"
-            type="text"
-            v-model="firstName"
-            required
-          />
-        </label>
+        <FormInput
+          v-model="firstName"
+          label="First name"
+          type="text"
+          :isFullWidth="false"
+          class="pr-1"
+        />
 
-        <label class="w-1/2 flex flex-col pl-1 mb-4">
-          Last name
-          <input
-            class="border rounded px-3 py-1 mt-2"
-            type="text"
-            v-model="lastName"
-            required
-          />
-        </label>
+        <FormInput
+          v-model="lastName"
+          label="Last name"
+          type="text"
+          :isFullWidth="false"
+          class="pl-1"
+        />
       </div>
 
-      <label class="flex flex-col mb-4">
-        Email
-        <input class="border rounded px-3 py-1 mt-2" type="email" v-model="email" required />
-      </label>
+      <FormInput v-model="email" label="Email" type="email" :isFullWidth="true" />
 
-      <label class="flex flex-col mb-4">
-        Password
-        <input
-          class="border rounded px-3 py-1 mt-2"
-          type="password"
-          v-model="password"
-          required
-        />
-      </label>
+      <FormInput v-model="password" label="Password" type="password" :isFullWidth="true" />
 
       <button class="block w-full bg-indigo-700 text-white font-bold rounded px-4 py-2">
         <span v-if="isSignup">Signup</span>
@@ -54,6 +39,7 @@
 </template>
 
 <script>
+import FormInput from './FormInput';
 import { mapGetters } from 'vuex';
 
 export default {
@@ -63,6 +49,10 @@ export default {
       type: Boolean,
       required: true
     }
+  },
+
+  components: {
+    FormInput
   },
 
   data() {
@@ -93,6 +83,7 @@ export default {
           password: this.password
         });
       }
+
       promise.then(() => {
         if (!this.authErr) {
           this.$router.push('/');
---
+++ client/src/components/FormInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <label class="block mb-4" :class="{'w-1/2': !isFullWidth}">
+    {{label}}
+    <input
+      @input="$emit('input', content)"
+      class="block w-full border rounded px-3 py-1 mt-2"
+      :type="type"
+      v-model="content"
+      required
+    />
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'FormInput',
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    isFullWidth: {
+      type: Boolean,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      content: this.value
+    };
+  }
+};
+</script>
